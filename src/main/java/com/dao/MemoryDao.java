8bef48a2d7b0a47632c5fc1b60064bcb2df07ac2
bbigcat111 <bbigcat111@163.com>
2020-08-31 21:56:54 +0800

    add a MemoryDao

--- src/main/java/com/dao/MemoryDao.java
+++ src/main/java/com/dao/MemoryDao.java
@@ -1,4 +1,15 @@
 package com.dao;
 
+import com.pojo.MemoryData;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface MemoryDao {
+    @Select("select * from memory where stu_id = #{stu_id}")
+    public MemoryData getMemoryDataById(String stu_id);
+
+    @Select("update memory set memoryData = #{memoryData} where stu_id = #{stu_id}")
+    public void setMemoryDataById(@Param("stu_id") String stu_id, @Param("memoryData") String memoryData);
 }
