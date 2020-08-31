30e117bdd036f4dc697358609bb25f91ab8f1aa2
bbigcat111 <bbigcat111@163.com>
2020-08-31 21:59:03 +0800

    add MemoryService

--- src/main/java/com/service/MemoryService.java
+++ src/main/java/com/service/MemoryService.java
@@ -1,4 +1,9 @@
 package com.service;
 
+import com.pojo.MemoryData;
+
 public interface MemoryService {
+    public MemoryData getMemoryDataById(String stu_id);
+
+    public void setMemoryDataById(String stu_id, String memoryData);
 }
