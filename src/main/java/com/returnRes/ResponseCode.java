5262d29243340d2777c6bc0074ed25d4a5697443
bbigcat111 <bbigcat111@163.com>
2020-08-31 22:06:05 +0800

    ResponseCode add a MEMORY

--- src/main/java/com/returnRes/ResponseCode.java
+++ src/main/java/com/returnRes/ResponseCode.java
@@ -4,7 +4,8 @@ public enum  ResponseCode {
     SUCCESS(0, "Login Success"),
     FAILURE(-1, "Login Fail"),
     COURSE(100, "Show Course"),
-    USER(200, "Get User")
+    USER(200, "Get User"),
+    MEMORY(300, "Get MemoryData")
     ;
 
     private int code;
