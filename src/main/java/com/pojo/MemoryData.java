318bbc6420dc3767041e18e1e0cc204db3f16f6e
bbigcat111 <bbigcat111@163.com>
2020-08-31 21:57:25 +0800

    add a MemoryData pojo

--- src/main/java/com/pojo/MemoryData.java
+++ src/main/java/com/pojo/MemoryData.java
@@ -1,4 +1,22 @@
 package com.pojo;
 
 public class MemoryData {
+    private String stu_id;
+    private String memoryData;
+
+    public String getStu_id() {
+        return stu_id;
+    }
+
+    public void setStu_id(String stu_id) {
+        this.stu_id = stu_id;
+    }
+
+    public String getMemoryData() {
+        return memoryData;
+    }
+
+    public void setMemoryData(String memoryData) {
+        this.memoryData = memoryData;
+    }
 }
