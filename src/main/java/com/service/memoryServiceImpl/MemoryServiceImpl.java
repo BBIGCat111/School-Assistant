0e05fefbd6161c64f353008b386bca068dbe9b87
bbigcat111 <bbigcat111@163.com>
2020-08-31 21:59:41 +0800

    add MemoryServiceImpl

--- src/main/java/com/service/memoryServiceImpl/MemoryServiceImpl.java
+++ src/main/java/com/service/memoryServiceImpl/MemoryServiceImpl.java
@@ -1,4 +1,19 @@
 package com.service.memoryServiceImpl;
 
-public class MemoryServiceImpl {
+import com.pojo.MemoryData;
+import com.service.MemoryService;
+import com.dao.MemoryDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemoryServiceImpl implements MemoryService {
+    @Autowired
+    private MemoryDao memoryDao;
+
+    public MemoryData getMemoryDataById(String stu_id){
+        return memoryDao.getMemoryDataById(stu_id);
+    }
+
+    public void setMemoryDataById(String stu_id, String memoryData) { memoryDao.setMemoryDataById(stu_id, memoryData); }
 }
