7c791c5aaaec2a266a1e1d05ed4ecff62c2a8a08
bbigcat111 <bbigcat111@163.com>
2020-08-31 21:55:29 +0800

    add a MemoryController

--- src/main/java/com/controller/MemoryController.java
+++ src/main/java/com/controller/MemoryController.java
@@ -1,4 +1,38 @@
 package com.controller;
 
+import com.pojo.MemoryData;
+import com.service.MemoryService;
+import com.returnRes.ServerResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
 public class MemoryController {
+    @Autowired
+    private MemoryService memoryService;
+
+    @RequestMapping("/memory.eo")
+    @ResponseBody
+    public ServerResponse<MemoryData> getMemoryDataById(String newMemoryData){
+
+        //获取sesson
+        RequestAttributes ra = RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = ((ServletRequestAttributes)ra).getRequest();
+        String stu_id = (String) request.getSession(true).getAttribute("stu_id");
+
+        if(newMemoryData != null){
+            memoryService.setMemoryDataById(stu_id, newMemoryData);
+        }
+
+        MemoryData memoryData = memoryService.getMemoryDataById(stu_id);
+
+        return ServerResponse.createMemoryResponse(memoryData);
+    }
 }
