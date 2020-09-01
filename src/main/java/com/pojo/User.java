1a6c1ed06d038ccd82915b3cc48080d0a7dfb252
bbigcat111 <bbigcat111@163.com>
2020-09-01 17:37:16 +0800

    update User

--- src/main/java/com/pojo/User.java
+++ src/main/java/com/pojo/User.java
@@ -4,8 +4,13 @@ public class User {
     private String stu_id;
     private String stu_name;
     private String stu_sex;
+    private Integer stu_age;
+    private String stu_political;
+    private String stu_address;
     private String stu_profess;
     private String stu_class;
+    private String stu_phone;
+    private String stu_email;
 
     public String getStu_id() {
         return stu_id;
@@ -31,6 +36,30 @@ public class User {
         this.stu_sex = stu_sex;
     }
 
+    public Integer getStu_age() {
+        return stu_age;
+    }
+
+    public void setStu_age(Integer stu_age) {
+        this.stu_age = stu_age;
+    }
+
+    public String getStu_political() {
+        return stu_political;
+    }
+
+    public void setStu_political(String stu_political) {
+        this.stu_political = stu_political;
+    }
+
+    public String getStu_address() {
+        return stu_address;
+    }
+
+    public void setStu_address(String stu_address) {
+        this.stu_address = stu_address;
+    }
+
     public String getStu_profess() {
         return stu_profess;
     }
@@ -45,5 +74,21 @@ public class User {
 
     public void setStu_class(String stu_class) {
         this.stu_class = stu_class;
+    }
+
+    public String getStu_phone() {
+        return stu_phone;
+    }
+
+    public void setStu_phone(String stu_phone) {
+        this.stu_phone = stu_phone;
+    }
+
+    public String getStu_email() {
+        return stu_email;
+    }
+
+    public void setStu_email(String stu_email) {
+        this.stu_email = stu_email;
     }
 }
