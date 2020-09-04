b5f981647fb040c9b4e4dc0c072496b4a6aac2cd
bbigcat111 <bbigcat111@163.com>
2020-09-04 14:42:42 +0800

    add a new method

--- src/main/java/com/service/userServiceImpl/UserServiceImpl.java
+++ src/main/java/com/service/userServiceImpl/UserServiceImpl.java
@@ -14,4 +14,9 @@ public class UserServiceImpl implements UserService{
     public User getUserById(String stu_id){
         return userDao.getUserById(stu_id);
     }
+
+    public void updateUserInfoById(String stu_id, String stu_age, String stu_political, String stu_address
+            , String stu_profess, String stu_class, String stu_phone, String stu_email){
+        userDao.updateUserInfoById(stu_id, stu_age, stu_political, stu_address, stu_profess, stu_class, stu_phone, stu_email);
+    }
 }
